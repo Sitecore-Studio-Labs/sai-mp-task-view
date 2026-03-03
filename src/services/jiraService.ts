c7aca686433249c21c3554cb3dbc00aca8ffcc01
Sanduni Galagoda <sdgg@sitecore.net>
2026-03-03 16:09:55 +0530

    feature: implement attachment GET route with dynamic attachmentId (#23)

--- src/services/jiraService.ts
+++ src/services/jiraService.ts
@@ -255,8 +255,16 @@ export const getDetailsForComment = async (
 
 export const createCommentForIssue = async (
   userId: UserId,
-  payload: CreateCommentPayload
+  payload: CreateCommentPayload,
 ): Promise<JiraComment> => {
   const { adapter, token } = await createJiraAdapterForUser(userId);
   return adapter.createComment(token, payload);
+};
+
+export const getAttachmentContent = async (
+  attachmentId: string,
+  userId: UserId,
+) => {
+  const { adapter, token } = await createJiraAdapterForUser(userId);
+  return adapter.getAttachmentContent(token, attachmentId);
 };
