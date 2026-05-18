6372d028d38fa5d01cc97f4ad3e756a86bc48358
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2026-05-18 14:58:30 +0530

    fix: removed current e2e impl

--- e2e/app.e2e.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test.describe("Home Page", () => {
-  test("should load the homepage", async ({ page }) => {
-    await page.goto("/");
-    const homeTitle = page.getByTestId("home-page-title");
-    await expect(homeTitle).toBeVisible();
-  });
-});
